1bf259a09646bd707af7141136f4171b30b81b71
jrcliff <justoon7.jc@gmail.com>
2020-11-17 21:40:22 -0500

    commit tree ff818e5ac3e116e7a24417d85024b02b1ea5fc24

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+function appendDriver(name){
+    let allDrivers = [...drivers];
+    allDrivers.push(name);
+    return allDrivers
+}
+
+function prependDriver(name){
+    let allDrivers = [...drivers]
+    allDrivers.unshift(name);
+    return allDrivers
+}
+
+function removeLastDriver(){
+    return drivers.slice(0, -1);
+}
+
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
